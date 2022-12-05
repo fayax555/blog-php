981c85a1b22a7e1001fc68a44e5c48e23bd516a9
Fayaz <fayax555@gmail.com>
2022-12-05 13:04:30 +0500

    update table icons

--- views/admin/categories/index.php
+++ views/admin/categories/index.php
@@ -3,7 +3,7 @@
     <tr class='[&>*]:px-5 [&>*]:text-left [&>*]:font-light'>
       <th class='py-2'>ID</th>
       <th>Name</th>
-      <th>Action</th>
+      <th class="translate-x-2">Action</th>
     </tr>
   </thead>
 
@@ -15,17 +15,18 @@
       <td class='py-4 font-semibold'><?= $category['name'] ?></td>
 
       <td class='rounded-r-md'>
-        <div class='flex items-center gap-4'>
-          <button type='button' title='edit category'>
+        <div class='flex items-center gap-2'>
+          <button type='button' title='edit category'
+            class="hover:bg-slate-300 p-2.5 transition [&:hover_svg]:text-blue-700 rounded-full">
             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024"
-              class="text-xl text-slate-600 transition hover:text-blue-600" height="1em" width="1em"
-              xmlns="http://www.w3.org/2000/svg">
+              class="text-xl text-slate-600 transitio" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M880 836H144c-17.7 0-32 14.3-32 32v36c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-36c0-17.7-14.3-32-32-32zm-622.3-84c2 0 4-.2 6-.5L431.9 722c2-.4 3.9-1.3 5.3-2.8l423.9-423.9a9.96 9.96 0 0 0 0-14.1L694.9 114.9c-1.9-1.9-4.4-2.9-7.1-2.9s-5.2 1-7.1 2.9L256.8 538.8c-1.5 1.5-2.4 3.3-2.8 5.3l-29.5 168.2a33.5 33.5 0 0 0 9.4 29.8c6.6 6.4 14.9 9.9 23.8 9.9z">
               </path>
             </svg>
           </button>
-          <button type='button' title='delete category'>
+          <button type='button' title='delete category'
+            class="hover:bg-slate-300 p-2.5 transition [&:hover_svg]:text-red-700 rounded-full">
             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024"
               class="text-xl text-slate-600 transition hover:text-red-600" height="1em" width="1em"
               xmlns="http://www.w3.org/2000/svg">
